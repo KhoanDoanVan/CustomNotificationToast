c571f45470066fca80aacc8e6b121b4bfc893974
khoanisdev <doanvankhoan124@gmail.com>
2024-12-25 19:02:43 +0700

    Initial Commit

---
+++ CustomNotificationToast/CustomNotificationToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNotificationToastApp.swift
+//  CustomNotificationToast
+//
+//  Created by Đoàn Văn Khoan on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomNotificationToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
